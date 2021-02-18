9545a972580c1a2c0d86e0018e8ba9b63da97d0e
Bruno André Scatena <bscatena@bestprojects.com.br>
2021-02-18 15:48:33 -0300

    Primeiro commit OO

---
+++ ContentContext/Career.cs
@@ -0,0 +1,7 @@
+namespace OO.ContentContext
+{
+    public class Career : Content
+    {
+        
+    }
+}
---
+++ ContentContext/Content.cs
@@ -0,0 +1,11 @@
+namespace OO.ContentContext
+{
+    public class Content
+    {
+        public int id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+    }
+}
