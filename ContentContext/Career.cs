e167ad1af334c14e7f815f5fcc8ef579222dea47
Bruno André Scatena <bscatena@bestprojects.com.br>
2021-02-24 19:02:01 -0300

    Expression body, nova carreira

--- ContentContext/Career.cs
+++ ContentContext/Career.cs
@@ -1,7 +1,19 @@
+using System.Collections.Generic;
+
 namespace OO.ContentContext
 {
     public class Career : Content
     {
-        public int Courses { get; set; }
+        public Career()
+        {
+            Items = new List<CareerItem>();
+        }
+        public IList<CareerItem> Items { get; set; }
+        //Expression Body
+        public int TotalCourses => Items.Count;
+        
+
     }
+
+    
 }
---
+++ ContentContext/CareerItem.cs
@@ -0,0 +1,15 @@
+namespace OO.ContentContext
+{
+
+    public class CareerItem
+    {
+        public int Ordem { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public Course course{ get; set; }
+    }
+
+
+}
